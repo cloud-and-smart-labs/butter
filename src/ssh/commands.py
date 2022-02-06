9a2cc059620cab5ce4456d5f582c65170a29d360
Suvam Basak <suvambasak2008@gmail.com>
2022-02-06 16:00:38 +0530

    Console loading and status show

--- src/ssh/commands.py
+++ src/ssh/commands.py
@@ -4,8 +4,18 @@ from src.ssh.printer import *
 
 
 def shell_command(inventory_name: str, cmd: str) -> None:
+    loader = Loading()
     for host in Inventory(inventory_name).get_inventory_list():
+        loader.start()
+
+        loader.set_status('Connecting...')
         ssh_connection = SSH(host[0], host[1], port=int(host[2]))
+
+        loader.set_status('Executing...')
         output = ssh_connection.command(cmd)
+
+        loader.set_status('Done.')
         ssh_connection.close()
+
+        loader.stop()
         command_output(output)
