7dc25b1b6993929a044d005cb86a985b354d5aed
Suvam Basak <suvambasak2008@gmail.com>
2022-02-19 22:10:16 +0530

    Concurrent execution of command with threading

--- src/ssh/commands.py
+++ src/ssh/commands.py
@@ -1,21 +1,32 @@
 from src.inventory.inventory import Inventory
 from src.ssh.ssh import SSH
 from src.ssh.printer import *
+import threading
 
 
 def shell_command(inventory_name: str, cmd: str) -> None:
-    loader = Loading()
-    for host in Inventory(inventory_name).get_inventory_list():
+    def executor(host: list, output_lock: threading.Lock) -> None:
+        loader = Loading()
         loader.start()
 
-        loader.set_status('Connecting...')
+        loader.set_status(f'Connecting [{host[0]}] ...')
         ssh_connection = SSH(host[0], host[1], port=int(host[2]))
 
-        loader.set_status('Executing...')
+        loader.set_status(f'Executing [{host[0]}] ...')
         output = ssh_connection.command(cmd)
 
-        loader.set_status('Done.')
+        loader.set_status(f'Done [{host[0]}] .')
         ssh_connection.close()
 
         loader.stop()
-        command_output(output)
+
+        with output_lock:
+            # print(f'lock acquire by: {threading.current_thread()}')
+            command_output(output)
+            # print(f'lock release by: {threading.current_thread()}')
+
+    output_lock = threading.Lock()
+    for host in Inventory(inventory_name).get_inventory_list():
+        threading.Thread(name=host[0],
+                         target=executor,
+                         args=(host, output_lock),).start()
