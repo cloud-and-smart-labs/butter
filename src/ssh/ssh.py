0b1026c9ecdb8c7a1e9fb49e1c0a174f85af6b3f
Suvam Basak <suvambasak2008@gmail.com>
2022-02-20 12:46:47 +0530

    New dataclass created

--- src/ssh/ssh.py
+++ src/ssh/ssh.py
@@ -1,4 +1,32 @@
 import paramiko
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Output:
+    '''
+    Dataclass for command execution details and outputs
+    '''
+    ssh_id: str
+    cmd: str
+    cmd_status: int
+    cmd_output: str
+    cmd_error: str
+
+    def get_ssh_id(self) -> str:
+        return self.ssh_id
+
+    def get_command(self) -> str:
+        return self.cmd
+
+    def get_status(self) -> int:
+        return self.cmd_status
+
+    def get_output(self) -> str:
+        return self.cmd_output
+
+    def get_error(self) -> str:
+        return self.cmd_error
 
 
 class SSH:
@@ -10,17 +38,19 @@ class SSH:
         self.client = paramiko.SSHClient()
         self.client.load_system_host_keys()
         self.client.connect(hostname=hostname, username=username, port=port)
-        self.cmd_output = self.Output(hostname, username)
+        self.ssh_id = f'{username}@{hostname}'
 
-    def command(self, command) -> object:
+    def command(self, command) -> Output:
         _, stdout, stderr = self.client.exec_command(
             command)
-        self.cmd_output.cmd = command
-        self.cmd_output.cmd_status = stdout.channel.recv_exit_status()
-        self.cmd_output.cmd_output = stdout.read().decode('utf8')
-        self.cmd_output.cmd_error = stderr.read().decode('utf8')
 
-        return self.cmd_output
+        return Output(
+            self.ssh_id,
+            command,
+            stdout.channel.recv_exit_status(),
+            stdout.read().decode('utf8'),
+            stderr.read().decode('utf8')
+        )
 
     def command_stream(self, command) -> None:
         stdin, stdout, stderr = self.client.exec_command(command)
@@ -32,26 +62,3 @@ class SSH:
 
     def close(self) -> None:
         self.client.close()
-
-    class Output:
-        def __init__(self, hostname, username) -> None:
-            self.ssh_id = f'{username}@{hostname}'
-            self.cmd_status = 0
-            self.cmd_output = ''
-            self.cmd_error = ''
-            self.cmd = ''
-
-        def get_ssh_id(self) -> str:
-            return self.ssh_id
-
-        def get_command(self) -> str:
-            return self.cmd
-
-        def get_status(self) -> int:
-            return self.cmd_status
-
-        def get_output(self) -> str:
-            return self.cmd_output
-
-        def get_error(self) -> str:
-            return self.cmd_error
